7b194d2d40d9723bd5f7d61d672bc25e4b6ac426
kylepothulUF <126925197+kylepothulUF@users.noreply.github.com>
2023-03-08 16:26:02 -0500

    Update main.py
    Added partner's name to top of program.
    Changed name of encoding function and ensured any digit in a password doesn't go over a single digit.
    Added decode function.
    Adjusted main loop to work with new decode function.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Aditya Khera
+
 # menu
 def print_menu():
     print()
@@ -11,24 +13,51 @@ def print_menu():
 
 
 # encodes password by string conversion, adding 3 for each digit and concatenating after through print statements
-def encode_password(password):
+def encode(password):
     output = 0
     str_output = ''
     for digit in str(password):
         if digit.isdigit():
             output = int(digit) + 3
+
+            # # if the value ends up higher than 9, reduce the value by 10
+            # so it stays a single digit
+            if output > 9:
+                output -= 10
+
             str_output += str(output)
+    print('Your password has been encoded and stored!')
     return str_output
 
 
+# decodes the password by subtracting 3 from each value, returning the decoded password
+def decode(encoded_password):
+    decoded_password = ''
+
+    for index in range(0, len(encoded_password)):
+        decoded_value = int(encoded_password[index]) - 3
+
+        # if the value becomes negative, add 10 to the value to keep it positive
+        if decoded_value < 0:
+            decoded_value += 10
+
+        decoded_password += str(decoded_value)
+
+    return decoded_password
+
+
+# loops the application menu until the user exits
 if __name__ == "__main__":
+    encoded_password = ''
+
     while True:
         print_menu()
         option = int(input())
         if option == 1:
             password = input("Please enter your password to encode: ")
-            print(encode_password(password))
+            encoded_password = encode(password)
         elif option == 2:
-            print("The encoded password is ", encode_password(password), ", and the original password is ", password, sep='')
+            decoded_password = decode(encoded_password)
+            print("The encoded password is ", encoded_password, ", and the original password is ", decoded_password, sep='')
         elif option == 3:
             break
